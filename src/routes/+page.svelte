de4c2ffe1e644d75c45702258983a19d1b750355
Naishar.Shah <naishar.shah@trivium-esolutions.con>
2024-02-23 11:58:30 +0530

    Basic svelte application which fetches data from external api and displays it in home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script context="module" lang="ts">
+    export async function load() {
+        const res = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=7a97c8bd3c7ee651f5f2866e7cf25411')
+        const data = await res.json();
+        if (res.ok) {
+            return data.results;
+        }
+        return {
+            status: res.status,
+            error: new Error()
+        };
+    }
+</script>
+
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    let responseData = '';
+
+    onMount(async () => {
+        try {
+            responseData = await load();
+        } catch (error) {
+            responseData = 'Error fetching data';
+        }
+    });
+</script>
+
+<h1>Data from API:</h1>
+
+{#await responseData}
+    <p>...waiting</p>
+{:then responseData}
+    {#each responseData as movie}
+        <p>The number is {movie.original_title}</p>
+    {/each}
+{:catch error}
+    <p style="color: red">{error.message}</p>
+{/await}
