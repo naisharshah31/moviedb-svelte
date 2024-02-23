530b84c0c9e85f7025185a501567d8f3af0c4ee6
Naishar.Shah <naishar.shah@trivium-esolutions.con>
2024-02-23 12:32:49 +0530

    Code refactoring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
-<script context="module" lang="ts">
-    export async function load() {
+<script>
+    import { onMount } from 'svelte';
+
+    let responseData = '';
+
+    onMount(async () => {
+        try {
+            responseData = await load();
+        } catch (error) {
+            responseData = 'Error fetching data';
+        }
+    });
+
+    async function load() {
         const res = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=7a97c8bd3c7ee651f5f2866e7cf25411')
         const data = await res.json();
         if (res.ok) {
@@ -12,20 +24,6 @@
     }
 </script>
 
-
-<script lang="ts">
-    import { onMount } from 'svelte';
-
-    let responseData = '';
-
-    onMount(async () => {
-        try {
-            responseData = await load();
-        } catch (error) {
-            responseData = 'Error fetching data';
-        }
-    });
-</script>
 
 <h1>Data from API:</h1>
 
